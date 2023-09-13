de81d915c91e07fd37a5188a22a787de4da58ab7
JulianaAstra <julianaas@yandex.ru>
2023-09-14 01:39:50 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import "./index.css";
 import App from './Components/app/app';
 import { StrictMode } from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
     <StrictMode>
